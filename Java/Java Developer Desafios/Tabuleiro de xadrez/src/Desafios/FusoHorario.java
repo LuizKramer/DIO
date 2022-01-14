9792d18a2498a2f223c4bde3a524826c3a9f28ac
LuizKramer <luizeduardockramer@gmail.com>
2022-01-14 10:08:18 -0300

    Adiciona desafio FusoHorario

---
+++ Java/Java Developer Desafios/Tabuleiro de xadrez/src/Desafios/FusoHorario.java
@@ -0,0 +1,22 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class FusoHorario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int horasaida = sc.nextInt();
+        int tempoviagem = sc.nextInt();
+        int fuso = sc.nextInt();
+        int ajuste;
+        ajuste = horasaida + tempoviagem + fuso;
+        if (ajuste >= 24)
+            ajuste -= 24;
+        else if (ajuste < 0)
+            ajuste += 24;
+
+        System.out.println(ajuste);
+        sc.close();
+    }
+}
