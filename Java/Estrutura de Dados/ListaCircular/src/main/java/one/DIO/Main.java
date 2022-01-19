96b0c9208017ab1d894f330be6be16745c8e2469
LuizKramer <luizeduardockramer@gmail.com>
2022-01-19 13:58:15 -0300

    Adiciona ListaCircular

--- Java/Estrutura de Dados/ListaCircular/src/main/java/one/DIO/Main.java
+++ Java/Estrutura de Dados/ListaCircular/src/main/java/one/DIO/Main.java
@@ -3,6 +3,7 @@ package one.DIO;
 public class Main {
     public static void main(String[] args) {
         ListaCircular<Integer> listaCircular = new ListaCircular<>();
+        System.out.println(listaCircular);
         listaCircular.add(1);
         listaCircular.add(2);
         listaCircular.add(3);
@@ -15,5 +16,6 @@ public class Main {
 
         System.out.println(listaCircular);
 
+        System.out.println(listaCircular.get(6));
     }
 }
